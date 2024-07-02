880b123015f16d25444ac44e5cd46f46e01ac716
Mehmet Bulut <mehmet.bulut02@hotmail.com>
2024-07-02 14:25:46 +0300

    tek çift örnek metot

---
+++ cift_tek_metot/cift_tek_metot/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cift_tek_metot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kullanıcıdan alınan sayı tek mi çift mi oldugunu bulan metot
+            Console.Write("Bir sayı giriniz: ");
+            int girilenSayi = int.Parse(Console.ReadLine());
+            tekmiCiftmi(girilenSayi);
+        }
+
+        static void tekmiCiftmi(int sayi)
+        {
+            if (sayi % 2 == 0)
+            {
+                Console.WriteLine("Sayı Çifttir");
+            }
+            else
+            {
+                Console.WriteLine("Sayı Tektir");
+            }
+            Console.ReadLine();
+        }
+    }
+}
