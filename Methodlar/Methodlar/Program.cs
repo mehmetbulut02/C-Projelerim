a6ca210e43989d58cd685876bf2e73d2c1bcca5c
Mehmet Bulut <mehmet.bulut02@hotmail.com>
2024-07-02 10:26:35 +0300

    Metotlar

---
+++ Methodlar/Methodlar/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methodlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Parametresiz - Geriye Değer Döndürmeyen Metot Tanımlama...
+            SelamVer();
+            AdiniYazdir();
+        }
+        static void SelamVer() 
+        {
+            Console.WriteLine("Selamlar :");
+        
+        }
+        static void AdiniYazdir()
+        {
+            Console.WriteLine("Mehmet");
+            Console.ReadLine();
+        }
+    }
+}
