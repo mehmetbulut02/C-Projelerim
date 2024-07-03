ef4aa9da12a0679fb97dcd7646b04106301fdb3b
Mehmet Bulut <mehmet.bulut02@hotmail.com>
2024-07-03 16:40:51 +0300

    Mini öğrenci bilgi uygulaması

---
+++ Ogrenci_Uygulaması/Ogrenci_Uygulaması/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ogrenci_Uygulaması
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool kontrol = true;
+            Öğrenci öğrenci1 = new Öğrenci(1, "Mehmet", "Bulut", 50, 40, 73, "İBU");
+
+            Console.WriteLine("Uygulamamıza hoşgeldiniz... Yapmak istediğiniz işlemi seçiniz");
+            
+
+            while (kontrol)
+            {
+                islemleriGoster();
+                string secim = Console.ReadLine();
+
+                switch (secim)
+                {
+                    case "1":
+                        öğrenci1.ogrenciBilgileriGoster();
+                        break;
+                    case "2":
+                        double ogrenciOrtalama = öğrenci1.ogrenciNotuBul();
+                        break;
+                    case "3":
+                        öğrenci1.okulGetir();
+                        break;
+                    case "4":
+                        kontrol = false;
+                        break;
+
+                }
+            }
+        }
+        static void islemleriGoster()
+        {
+            Console.WriteLine("1- Öğrenci Bilgileri Göster");
+            Console.WriteLine("2- Öğrenci Ortalamasını Göster");
+            Console.WriteLine("3- Öğrenci Okulunu Göster");
+            Console.WriteLine("4- Çıkış Yap");
+        }
+    }
+}
---
+++ Ogrenci_Uygulaması/Ogrenci_Uygulaması/Öğrenci.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ogrenci_Uygulaması
+{
+    internal class Öğrenci
+    {
+        private int ogrno;
+        private string isim;
+        private string soyisim;
+        private int vize1;
+        private int vize2;
+        private int final;
+        private string okulismi;
+
+        public Öğrenci(int _ogrno , string _isim , string _soyisim , int _vize1, int _vize2, int _final , string _okulismi)
+        {
+            ogrno = _ogrno;
+            isim = _isim;
+            soyisim = _soyisim;
+            vize1 = _vize1;
+            vize2 = _vize2;
+            final = _final;
+            okulismi = _okulismi;
+        }
+
+        public void ogrenciBilgileriGoster()
+        {
+            Console.WriteLine("Öğrenci Numarası: "+ ogrno);
+            Console.WriteLine("Öğrenci İsmi: "+ isim);
+            Console.WriteLine("Öğrenci Vize1 : " + vize1);
+            Console.WriteLine("Öğrenci Vize2 : " + vize2 );
+            Console.WriteLine("Öğrenci final : " + final);
+            Console.WriteLine("Öğrenci Okul İsmi : " +okulismi);
+        }
+
+        public double ogrenciNotuBul()
+        {
+          double ortalama =  vize1 * 0.2 + vize2 * 0.2 + final * 0.6;
+            Console.WriteLine("Öğrencinin Ortalaması : " + ortalama); 
+            return ortalama;
+        }
+        public void okulGetir()
+        { 
+            Console.WriteLine("Öğrencinin Okul İsmi : " + okulismi);
+        }
+    }
+}
