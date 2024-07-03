e9169877f84976381cfa6b258263a6537efd8a73
Mehmet Bulut <mehmet.bulut02@hotmail.com>
2024-07-03 15:11:16 +0300

    Public ve private kavramları

---
+++ public_private/public_private/Müşteri.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace public_private
+{
+    public class Müşteri
+    {
+        public string isim;
+        public string soyisim;
+        public int maasDegeri;
+        public string cinsiyet;
+        private int yas;
+
+        public Müşteri() 
+        {
+            Console.WriteLine("Müşteri sınıfının yapıcı metodu çalıştı");
+        }
+        public void musteriBilgileriGoster()
+        {
+            Console.WriteLine("İsmi : " + isim);
+            Console.WriteLine("Soyisim : "+soyisim);
+            Console.WriteLine("Maaş Değeri : "+maasDegeri);
+            Console.WriteLine("Cinsiyet : "+cinsiyet);
+            Console.WriteLine("Yaş Değeri : "+yas);
+        }
+
+        private void isimSoyisimYazdır(string musteriisim, string musteriSoyisim)
+        {
+            Console.WriteLine("Müşterinin isim ve soyisimi :"+musteriisim+" "+musteriSoyisim);
+        }
+    }
+}
---
+++ public_private/public_private/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace public_private
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Müşteri müşteri1 = new Müşteri();
+
+            müşteri1.isim = "Mehmet";
+            müşteri1.soyisim = "Bulut";
+            müşteri1.maasDegeri = 2500;
+            müşteri1.cinsiyet = "Erkek";
+
+            müşteri1.musteriBilgileriGoster();
+
+        }
+    }
+}
