8aef72bb796053d90d489f872dd6b569d407d28a
Mehmet Bulut <mehmet.bulut02@hotmail.com>
2024-07-03 10:43:11 +0300

    Class

---
+++ ClassKullanimi/ClassKullanimi/Araba.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassKullanimi
+{
+    internal class Araba
+    {
+        
+            public int kapiSayisi;
+            public string arabaModel;
+
+            public string arabaRengi;
+
+        public Araba(int _kapiSayisi , string _arabaModel , string _arabaRengi)
+        {
+            kapiSayisi = _kapiSayisi ;
+            arabaModel = _arabaModel ;
+            arabaRengi = _arabaRengi ;
+        }
+
+        public void motorCalistir()
+            {
+                Console.WriteLine("Motor Çalışıyor...");
+            }
+            public void kapilariKilitle()
+            {
+                Console.WriteLine("Kapılar Kilitleniyor...");
+            }
+        
+    }
+
+
+}
---
+++ ClassKullanimi/ClassKullanimi/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassKullanimi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Araba araba1 = new Araba(4,"Renault","Kırmızı");
+
+            Console.WriteLine("Arabanın Rengi : " + araba1.arabaRengi);
+            Console.WriteLine("Araba Modeli : "+ araba1.arabaModel);
+            Console.WriteLine("Kapı Sayısı : "+ araba1.kapiSayisi);
+
+            araba1.kapilariKilitle();
+            araba1.motorCalistir();
+
+            Console.WriteLine("Arabanın kapı sayısı :"+ araba1.kapiSayisi);
+            Console.WriteLine("Araba Rengi: "+araba1.arabaRengi);
+            Console.WriteLine("Arabanın Modeli: "+araba1.arabaModel);
+        
+            Console.ReadLine();
+        }
+
+    }
+}
