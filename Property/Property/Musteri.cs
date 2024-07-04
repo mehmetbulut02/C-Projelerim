5a71af04e980b2bc01ddb7397782eddfb967799b
Mehmet Bulut <mehmet.bulut02@hotmail.com>
2024-07-04 09:51:31 +0300

    Property kavramına giriş yapıldı

---
+++ Property/Property/Musteri.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Property
+{
+    public class Musteri
+    {
+        private int kilo;
+
+        public int KİLO
+        {
+            get
+            {
+                return kilo;
+            }
+            set 
+            {
+               kilo= value;
+
+            }
+        }
+    }
+}
---
+++ Property/Property/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Property
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.KİLO = 85; // Değer atama yapıyorum o zaman set Blockları çalışır
+            // Eğer değer ataması yapmasaydım o zamanda get blockları çalışırdı
+            Console.WriteLine(musteri1.KİLO);  
+        }
+    }
+}
