88082918a8893b6514f9fe0347bf86348f0e700a
Mehmet Bulut <mehmet.bulut02@hotmail.com>
2024-07-04 14:10:33 +0300

    Static metot kullanımı ögrenildi

---
+++ StaticMetotKullanimi/StaticMetotKullanimi/Personel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMetotKullanimi
+{
+    public class Personel
+    {
+        public static void isimSoyisimyazdır()
+        {
+            System.Console.WriteLine("Mehmet Bulut");
+        }
+
+        public static void sayilariTopla(int sayi1, int sayi2)
+        {
+            Console.WriteLine("İki sayinin toplami : " + (sayi1 + sayi2));
+        }
+    }
+}
---
+++ StaticMetotKullanimi/StaticMetotKullanimi/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMetotKullanimi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Personel.isimSoyisimyazdır();
+            Personel.sayilariTopla(45, 6);
+            Console.ReadLine();
+        }
+    }
+}
