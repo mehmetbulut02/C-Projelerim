996d01993a3c771288bdcaa49797dd1ebedf4467
Mehmet Bulut <mehmet.bulut02@hotmail.com>
2024-07-04 09:22:30 +0300

    Kapsülleme

---
+++ KapsullemeyeGiris/KapsullemeyeGiris/Ogrenci.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KapsullemeyeGiris
+{
+    public class Ogrenci
+    {
+        private string isim ;
+
+        public void setIsım(string _isim)
+        {
+            isim = _isim;
+        }
+        public string getIsim() 
+        { 
+        return isim;
+        }
+    }
+}
---
+++ KapsullemeyeGiris/KapsullemeyeGiris/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KapsullemeyeGiris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci ogrenci1 = new Ogrenci();
+
+            ogrenci1.setIsım("Mehmet");
+            ogrenci1.getIsim();
+            
+        }
+    }
+}
