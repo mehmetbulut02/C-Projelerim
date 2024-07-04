7b4e67c1e482e9cfd09a3c33962b67c25ac3902a
Mehmet Bulut <mehmet.bulut02@hotmail.com>
2024-07-04 10:54:03 +0300

    Kapsülleme konusuna ait uygulama yapıldı

---
+++ kapsulleme_uygulama/kapsulleme_uygulama/Personel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kapsulleme_uygulama
+{
+    public class Personel
+    {
+        private string tckimlikno;
+
+        public string TCKİMLİKNO
+        {
+            get
+            {
+                return tckimlikno.Substring(0, 5)+"*******";
+            }
+            set
+            {
+                bool kontrol = false;
+                if (value.Length == 11)
+                {
+                    for (int i = 0; i<value.Length; i++)
+                    {
+                        bool sayiMi = char.IsNumber(value[i]);
+                        if (sayiMi) 
+                        { 
+                            // Yapmamız gereken bir şey yok
+                        }
+                        else
+                        {
+                            kontrol = true;
+                            break;
+                        }
+                    }
+                    if (kontrol)
+                    {
+                        Console.WriteLine("Tc kimlik numaranızda geçersiz karakter bulundu");
+                    }
+                    else
+                    {
+                        tckimlikno = value;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Girdiğiniz TC KİMLİK NO 11 karakter değildir.");
+                }
+            }
+        }
+    }
+}
---
+++ kapsulleme_uygulama/kapsulleme_uygulama/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kapsulleme_uygulama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Personel P1 = new Personel();
+            P1.TCKİMLİKNO = "12345678956";
+            Console.WriteLine("Tc kimlik numaranız : +"+P1.TCKİMLİKNO);
+        }
+    }
+}
