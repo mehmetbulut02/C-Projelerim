d4de11149f3e01bcaf4c7237f46ccbe2e784cb8d
Mehmet Bulut <mehmet.bulut02@hotmail.com>
2024-07-04 14:46:36 +0300

    Static değişkeni kullanımı öğrenildi

---
+++ StaticDegiskenKullanimi/StaticDegiskenKullanimi/Calısan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticDegiskenKullanimi
+{
+    public class Calısan
+    {
+        public int calisanID;
+        public string isim;
+        public string soyisim;
+        
+        public static int maas;
+        
+    }
+}
---
+++ StaticDegiskenKullanimi/StaticDegiskenKullanimi/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticDegiskenKullanimi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calısan c1 = new Calısan();
+
+            c1.calisanID= 1;
+            c1.isim = "Mehmet";
+            c1.soyisim = "Bulut";
+            Calısan.maas = 2500;
+            Console.WriteLine("Çalışan id : " + c1.calisanID);
+            Console.WriteLine("Çalışan isim : " + c1.isim);
+            Console.WriteLine("Çalışan soyisim : "+ c1.soyisim);
+            Console.WriteLine("Çalışan Maaşı : " + Calısan.maas);
+            Console.ReadLine();
+               
+        }
+    }
+}
