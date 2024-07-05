0a02ffa39b9176f143c2abb12c184b020313027c
Mehmet Bulut <mehmet.bulut02@hotmail.com>
2024-07-05 09:21:29 +0300

    Static Yapıcı metot kullanımı ogrenildi

---
+++ StaticYapiciMetotKullanimi/StaticYapiciMetotKullanimi/Kullanici.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticYapiciMetotKullanimi
+{
+    public class Kullanici
+    {
+        private int kullaniciID { get; set; }
+        private int isim { get; set; }
+        private int soyisim { get; set; }
+        private static int maas { get; set; }
+
+        static Kullanici()
+        {
+            maas = 2500;
+        }
+        public Kullanici(int kullaniciid , string isim , string soyisim)
+        {
+            kullaniciID = kullaniciid;
+            isim = isim;
+            soyisim = soyisim;
+        }
+        public void bilgileriGoster()
+        {
+            Console.WriteLine("Kullanici Bilgileri");
+            Console.WriteLine("Id : " + kullaniciID);
+            Console.WriteLine("İsim : " + isim);
+            Console.WriteLine("Soyisim : " + soyisim);
+            Console.WriteLine("Maaş : " + maas);
+        }
+
+        public void zamYap(int zamMiktari)
+        { 
+            Console.WriteLine("Kullanıcıya Zam Yapılıyor...");
+            Console.WriteLine("Şuanki Maaşı : " + (maas+zamMiktari));
+        }
+    }
+}
---
+++ StaticYapiciMetotKullanimi/StaticYapiciMetotKullanimi/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticYapiciMetotKullanimi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kullanici K1 = new Kullanici(1,"Mehmet","Bulut");
+            K1.bilgileriGoster();
+            K1.zamYap(700);
+            Console.ReadLine();
+        }
+    }
+}
